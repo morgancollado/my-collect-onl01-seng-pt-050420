05bc2c45d395417544c849f509a6a8e379e324cc
Morgan Collado <morgan.collado@gmail.com>
2020-05-12 14:38:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,11 @@
 def my_collect(array)
-i = 0
-collection = []
-while i < array.length
+ i = 0
+  collection = []
+  while i < array.length
+    collection = << yield(array[i])
+    i += 1 
+  end 
+  collection 
 end 
   
 
